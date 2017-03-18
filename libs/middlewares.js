fa1a9d336a668dd9cd1156046d8f1e52a04f1e89
Richard Dimalanta <chardmd.au@gmail.com>
2017-03-18 16:16:34 +1100

    CRUD for tasks’ endpoints.

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,15 @@
+import bodyParser from 'body-parser';
+
 module.exports = app => {
+
     app.set('port', 3000);
     app.set('json spaces', 4);
+
+    app.use(bodyParser.json());
+
+    app.use((req, res, next) => {
+        //middleware for preexecution routes
+        delete req.body.id;
+        next();
+    });
 };
