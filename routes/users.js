5b0b08011294832d2fb277750f900ff21765ae47
Richard Dimalanta <chardmd.au@gmail.com>
2017-03-18 17:37:28 +1100

    Added CRUD endpoints for Users.

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+module.exports = app => {
+
+    const Users = app.db.models.Users;
+
+    app.get('/users/:id', (req, res) => {
+        // '/tasks/1': Find a task
+        Users.findById(req.params.id, {
+            attributes: ['id', 'name', 'email']
+        })
+        .then(result => {
+            if (result) {
+                res.json(result);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(error=> {
+           res.status(412).json({msg: error.message});
+        });
+    });
+
+    app.delete('/users/:id', (req, res) => {
+        // 'tasks/1': Delete a task
+        Users.destroy({where: req.params.id})
+            .then(result => res.sendStatus(204))
+            .catch(error => {
+                res.status(412).json({msg: error.message});
+            });
+    });
+
+    app.post('/users/', (req, res) => {
+        // 'tasks/1': Update a task
+        Users.create(req.body)
+            .then(result => {
+                res.json(result);
+            })
+            .catch(error => {
+                res.status(412).json({msg: error.message});
+            });
+    });
+
+};
